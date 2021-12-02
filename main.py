7a8a0757b206c63e4ad2663d7f91a0cf0e0c8c70
wielorybek <63021070+wielorybek@users.noreply.github.com>
2021-12-02 01:33:07 +0100

    task 1 completed

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(stars):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Score: {stars}/50 stars')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi(2)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
